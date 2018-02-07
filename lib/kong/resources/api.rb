cbbeb5a23eb33e8d50e7b9569d62e7327fc83975
Nicolás De los Santos Cicutto <nico@acidtango.com>
2018-02-07 16:26:37 +0100

    Added missing attributes to APIs

--- lib/kong/resources/api.rb
+++ lib/kong/resources/api.rb
@@ -8,7 +8,8 @@ module Kong
     class API < Base
       attr_accessor :id, :name, :created_at, :strip_uri, :http_if_terminated, :https_only,
                     :upstream_url, :uris, :preserve_host, :upstream_connect_timeout,
-                    :upstream_read_timeout, :upstream_send_timeout, :retries
+                    :upstream_read_timeout, :upstream_send_timeout, :retries, :methods,
+                    :hosts
     end
   end
 end
